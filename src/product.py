502979b1bdd5e4dd9c44bc91a2e99102619b7c85
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-04-14 14:35:17 +0300

    add product.py and category.py

---
+++ src/product.py
@@ -0,0 +1,20 @@
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+if __name__ == '__main__':
+    prod1 = Product('Sony', 'rt-123', 123.0, 2)
+    prod2 = Product('Phillips', 'yu-123', 156.0, 1)
+    prod3 = Product('JVC', 'KL-123', 563.0, 8)
+

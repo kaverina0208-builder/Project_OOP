596d152e16091bd5b67dca96e641b1d80350ff19
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-04-14 15:30:04 +0300

    add test_product.py and test_category.py

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from src.product import Product
+from src.category import Category
+
+
+@pytest.fixture
+def first_cat():
+    return Category(
+        name='televisions',
+        description='3D',
+        product=[
+            Product('Sony', 'rt-123', 123.0, 2),
+            Product('Phillips', 'yu-123', 156.0, 1)
+        ]
+    )
+
+
+@pytest.fixture
+def second_cat():
+    return Category(
+        name='fridge',
+        description='embedded',
+        product=[
+            Product('Sony', '12-29', 258.0, 2),
+            Product('Sharp', 'RT-13', 966.0, 1),
+            Product('Phillips', '26-GH3', 566.0, 1)
+        ]
+    )
+
+
+@pytest.fixture
+def product():
+    return Product('JVC', 'HG-13', 256.0, 1)

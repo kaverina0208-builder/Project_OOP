0732c647ca0eb80b65ae41b2cc79db0d03a45aea
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-04-30 13:34:42 +0300

    add tests

---
+++ tests/test_print_mixin.py
@@ -0,0 +1,18 @@
+from src.product import Product
+from src.smartphone import Smartphone
+from src.lawngrass import LawnGrass
+
+
+def test_print_mixin(capsys):
+    Product("JVC", "HG-13", 256.0, 1)
+    massage = capsys.readouterr()
+    assert massage.out.strip() == 'Product(JVC, HG-13, 256.0, 1)'
+
+    Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+    massage = capsys.readouterr()
+    assert massage.out.strip() == 'Smartphone(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)'
+
+    LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    massage = capsys.readouterr()
+    assert massage.out.strip() == 'LawnGrass(Газонная трава, Элитная трава для газона, 500.0, 20)'
